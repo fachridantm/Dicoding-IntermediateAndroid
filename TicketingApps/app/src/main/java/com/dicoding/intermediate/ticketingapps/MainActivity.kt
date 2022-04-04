4d681fc3f145c7a3c786baab2a17fbafde1f0196
fachridantm <fachridantm@gmail.com>
2022-04-04 22:20:04 +0700

    ✨ Create listener for SeatView

--- TicketingApps/app/src/main/java/com/dicoding/intermediate/ticketingapps/MainActivity.kt
+++ TicketingApps/app/src/main/java/com/dicoding/intermediate/ticketingapps/MainActivity.kt
@@ -2,10 +2,29 @@ package com.dicoding.intermediate.ticketingapps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+
         setContentView(R.layout.activity_main)
+
+        val seatView = findViewById<SeatsView>(R.id.seats_view)
+        val button = findViewById<Button>(R.id.btn_finish)
+
+        button.setOnClickListener {
+            seatView.seat?.let {
+                Toast.makeText(this, "Kursi Anda nomor ${it.name}.", Toast.LENGTH_SHORT).show()
+            } ?: run {
+                Toast.makeText(this, "Silakan pilih kursi terlebih dahulu.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
