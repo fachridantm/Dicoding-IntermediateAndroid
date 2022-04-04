61310dc2e76f48fc055ced7cf26b3189b17ca009
fachridantm <fachridantm@gmail.com>
2022-04-04 22:17:30 +0700

    ✨ Creating data class

---
+++ TicketingApps/app/src/main/java/com/dicoding/intermediate/ticketingapps/Seat.kt
@@ -0,0 +1,9 @@
+package com.dicoding.intermediate.ticketingapps
+
+data class Seat(
+    val id: Int,
+    var x: Float? = 0F,
+    var y: Float? = 0F,
+    var name: String,
+    var isBooked: Boolean
+)
