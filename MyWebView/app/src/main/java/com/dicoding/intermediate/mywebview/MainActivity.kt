e6d16f5ae574ffc05c42e03a3da461f70b9c20a5
fachridantm <fachridantm@gmail.com>
2022-04-05 22:46:00 +0700

    ✨ Init commit - MyWebView

---
+++ MyWebView/app/src/main/java/com/dicoding/intermediate/mywebview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dicoding.intermediate.mywebview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
