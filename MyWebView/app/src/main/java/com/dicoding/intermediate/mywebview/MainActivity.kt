93119a148841768f35dfc97fd847f29b6bee76ff
fachridantm <fachridantm@gmail.com>
2022-04-07 12:18:48 +0700

    ✨ Update project MyWebView

--- MyWebView/app/src/main/java/com/dicoding/intermediate/mywebview/MainActivity.kt
+++ MyWebView/app/src/main/java/com/dicoding/intermediate/mywebview/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.dicoding.intermediate.mywebview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.intermediate.mywebview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.webView.apply {
+            true.also { settings.javaScriptEnabled = it }
+
+            webViewClient = object : WebViewClient() {
+                override fun onPageFinished(view: WebView, url: String) {
+                    view.loadUrl("javascript:alert('Web Dicoding berhasil dimuat')")
+                }
+            }
+
+            webChromeClient = object : WebChromeClient() {
+                override fun onJsAlert(view: WebView, url: String, message: String, result: android.webkit.JsResult): Boolean {
+                    Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+                    result.confirm()
+                    return true
+                }
+            }
+
+            loadUrl("https://www.dicoding.com")
+        }
     }
 }
