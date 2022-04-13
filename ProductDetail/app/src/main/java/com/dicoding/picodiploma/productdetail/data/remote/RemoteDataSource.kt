a3b0b9e9621eda76f49a5501338492ec3f4196b2
fachridantm <fachridantm@gmail.com>
2022-04-13 14:30:52 +0700

    ✨ Finishing - Product Detail

---
+++ ProductDetail/app/src/main/java/com/dicoding/picodiploma/productdetail/data/remote/RemoteDataSource.kt
@@ -0,0 +1,23 @@
+package com.dicoding.picodiploma.productdetail.data.remote
+
+import android.content.Context
+import com.dicoding.picodiploma.productdetail.R
+import com.dicoding.picodiploma.productdetail.data.model.ProductModel
+
+class RemoteDataSource(private val context: Context) {
+
+    fun getDetailProduct(): ProductModel {
+        return ProductModel(
+            name = context.getString(R.string.name),
+            store = context.getString(R.string.store),
+            size = context.getString(R.string.size),
+            color = context.getString(R.string.color),
+            desc = context.getString(R.string.description),
+            image = R.drawable.shoes,
+            date = context.getString(R.string.date),
+            rating = context.getString(R.string.rating),
+            price = context.getString(R.string.price),
+            countRating = context.getString(R.string.countRating)
+        )
+    }
+}
---
+++ ProductDetail/app/src/main/java/com/dicoding/picodiploma/productdetail/view/main/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.dicoding.picodiploma.productdetail.view.main
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.picodiploma.productdetail.R
+import com.dicoding.picodiploma.productdetail.data.model.ProductModel
+import com.dicoding.picodiploma.productdetail.data.remote.RemoteDataSource
+import com.dicoding.picodiploma.productdetail.databinding.ActivityMainBinding
+import com.dicoding.picodiploma.productdetail.helper.FormatHelper.withCurrencyFormat
+import com.dicoding.picodiploma.productdetail.helper.FormatHelper.withDateFormat
+import com.dicoding.picodiploma.productdetail.helper.FormatHelper.withNumberingFormat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+        setupAction()
+        setupData()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction() {
+        binding.settingImageView.setOnClickListener {
+            startActivity(Intent(Settings.ACTION_LOCALE_SETTINGS))
+        }
+    }
+
+    private fun setupData() {
+        val data = RemoteDataSource(this)
+        val product = data.getDetailProduct().apply {
+            binding.apply {
+                previewImageView.setImageResource(image)
+                nameTextView.text = name
+                storeTextView.text = store
+                colorTextView.text = color
+                sizeTextView.text = size
+                descTextView.text = desc
+                priceTextView.text = price.withCurrencyFormat()
+                dateTextView.text = getString(R.string.dateFormat, date.withDateFormat())
+                ratingTextView.text = getString(
+                    R.string.ratingFormat,
+                    rating.withNumberingFormat(),
+                    countRating.withNumberingFormat()
+                )
+            }
+        }
+        setupAccessibility(product)
+    }
+
+    private fun setupAccessibility(product: ProductModel) {
+        product.apply {
+            binding.apply {
+                settingImageView.contentDescription = getString(R.string.settingDescription)
+                previewImageView.contentDescription = getString(R.string.previewDescription)
+                colorTextView.contentDescription = getString(R.string.colorDescription, color)
+                sizeTextView.contentDescription = getString(R.string.sizeDescription, size)
+                ratingTextView.contentDescription = getString(
+                    R.string.ratingDescription,
+                    rating.withNumberingFormat(),
+                    countRating.withNumberingFormat()
+                )
+                storeTextView.contentDescription = getString(R.string.storeDescription, store)
+            }
+        }
+    }
+}
