53d951b1bb9fb6089b148270bea8b2320510cad5
fachridantm <fachridantm@gmail.com>
2022-05-17 21:16:20 +0700

    ✨ Add new service for Google Maps API - StoryApp

--- StoryApp/app/src/main/java/com/dicoding/intermediate/storyapp/service/api/ApiService.kt
+++ StoryApp/app/src/main/java/com/dicoding/intermediate/storyapp/service/api/ApiService.kt
@@ -1,9 +1,6 @@
 package com.dicoding.intermediate.storyapp.service.api
 
-import com.dicoding.intermediate.storyapp.service.response.AddStoryResponse
-import com.dicoding.intermediate.storyapp.service.response.LoginResponse
-import com.dicoding.intermediate.storyapp.service.response.RegisterResponse
-import com.dicoding.intermediate.storyapp.service.response.StoriesResponse
+import com.dicoding.intermediate.storyapp.service.response.*
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
@@ -37,4 +34,10 @@ interface ApiService {
         @Part file: MultipartBody.Part,
         @Part("description") description: RequestBody
     ):Call<AddStoryResponse>
+
+    @GET("stories")
+    fun getListStorieswithLocation(
+        @Header("Authorization") token: String,
+        @Query("location") loc : Int = 1
+    ):Call<StoriesResponseWithLocation>
 }
